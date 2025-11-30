4d8387d222d823b573579e27993a5d5789e92fa1
ValentinMICHELiutnfc <valentin.michel03@edu.univ-fcomte.fr>
2025-11-30 14:37:51 +0100

    mouvement amélioré

--- FPS_Zombie/Assets/Scripts/PlayerController.cs
+++ FPS_Zombie/Assets/Scripts/PlayerController.cs
@@ -5,19 +5,28 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float _speed = 3.0f;
     private float rotationSpeed = 2.0f;
+    private Rigidbody _rigidbody;
 
     private PlayerInputController _playerInputController;
 
     private void Awake()
     {
         _playerInputController = GetComponent<PlayerInputController>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         Vector3 positionChange = new Vector3(_playerInputController.MovementInputVector.x , 0 , _playerInputController.MovementInputVector.y) * Time.deltaTime * _speed;
-        //Vector3 positionChange = new Vector3(_playerInputController.MovementInputVector.x , 0 , _playerInputController.MovementInputVector.y) * Time.deltaTime;
+        // Vector3 positionChange = new Vector3(_playerInputController.MovementInputVector.x , 0 , _playerInputController.MovementInputVector.y) * Time.deltaTime;
         transform.Translate (positionChange);
         transform.Rotate(0, rotationSpeed*Input.GetAxis("Mouse X"), 0);
+        
+
+        // Vector3 velocity = new Vector3(_playerInputController.MovementInputVector.x,0,_playerInputController.MovementInputVector.y)* _speed;
+
+        // velocity.y = _rigidbody.linearVelocity.y;
+
+        // _rigidbody.linearVelocity = velocity;
     }
 }
