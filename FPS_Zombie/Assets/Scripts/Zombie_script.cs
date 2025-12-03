4b8bc3812b822daaef8eb384aa599c5cfdb45d6c
ODufossez <oscardufossez@gmail.com>
2025-12-03 14:16:42 +0100

    Pathfinding ok

--- FPS_Zombie/Assets/Scripts/Zombie_script.cs
+++ FPS_Zombie/Assets/Scripts/Zombie_script.cs
@@ -7,17 +7,22 @@ public class Zombie_script : MonoBehaviour
 
     public float life = 100;
     
-    public float damage = 10;
+    public float damage = 1;
+
+    public GameObject target;
+
+    public  UnityEngine.AI.NavMeshAgent agent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.destination = target.transform.position;
     }
 
     private void OnCollisionEnter(Collision collision)
